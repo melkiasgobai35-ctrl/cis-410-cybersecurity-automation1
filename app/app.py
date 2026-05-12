c6f14d53a81278d10b0da121d0e7c40a03607992
melkiasgobai35 <melkiasgobai35@gmail.com>
2026-05-12 11:59:15 -0700

    app.py

--- app/app.py
+++ app/app.py
@@ -129,3 +129,6 @@ def health():
 if __name__ == '__main__':
     # FIX 3: debug=False
     app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
+
