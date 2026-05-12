880db0fe711ba15a64c0e69ac2a337bb607df97e
melkiasgobai35 <melkiasgobai35@gmail.com>
2026-05-12 11:58:19 -0700

    app.py

--- vulnerable_app/app.py
+++ vulnerable_app/app.py
@@ -78,4 +78,4 @@ if __name__ == '__main__':
     # VULNERABILITY: debug=True — never use in production
     app.run(host='0.0.0.0', port=5000, debug=True)
     app.run(host='0.0.0.0', port=5001, debug=True)
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5002, debug=True)
